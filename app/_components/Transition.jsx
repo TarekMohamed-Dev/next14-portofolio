1d720b5e86be9e47e23b82fc0eb440e05fc1e136
Tarek Mohamed <109057697+TarekMohamed-Dev@users.noreply.github.com>
2024-05-24 08:06:40 +0300

    1

--- app/_components/Transition.jsx
+++ app/_components/Transition.jsx
@@ -3,25 +3,20 @@ import { motion } from 'framer-motion';
 // Variants
 const transitionVariants = {
   initial: {
-    x: '100%'
+    x: '100%',
+    width: '100%',
   },
   animate: {
     x: '0%',
-    transition: {
-      duration: 0.6,
-      ease: 'easeInOut'
-    }
+    width: '100%',
   },
   exit: {
     x: ['0%', '100%'],
-    transition: {
-      duration: 0.6,
-      ease: 'easeInOut'
-    }
+    width: ['100%', '0%'],
   },
 };
 
-const Transition = () => {
+const Transition = ({ isExit }) => {
   return (
     <>
       <motion.div
@@ -30,6 +25,9 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
+        transition={{delay: 0.2, duration: 0.8, ease: 'easeInOut' }}
+        key="layer1"
+        isExit={isExit}
       />
       <motion.div
         className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]'
@@ -37,7 +35,9 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.1 }}
+        transition={{ delay: 0.4, duration: 0.8, ease: 'easeInOut' }}
+        key="layer2"
+        isExit={isExit}
       />
       <motion.div
         className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]'
@@ -45,7 +45,9 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.2 }}
+        transition={{ delay: 0.6, duration: 0.8, ease: 'easeInOut' }}
+        key="layer3"
+        isExit={isExit}
       />
     </>
   );
